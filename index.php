c42930f3de2b6a9ee35f0eaae6de063d34c9973a
Drownr <33448811+Drownr@users.noreply.github.com>
2017-11-20 22:00:04 +0100

    Cookie v0.5

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
-<?php 
-	$contatore = 0;
-	setcookie("Biscotto", $contatore);
-	echo "Biscotto settato! Primo accesso eseguito ;) <br>";
-
-?>
-
 <html>
  <head>
   <title>Contatore Accessi :D</title>
  </head>
  <body>
+	<?php 
+	if (!isset($_COOKIE["Biscotto"])){
+		$contatore = 0;
+		setcookie("Biscotto", $contatore);
+		echo "Biscotto settato! Primo accesso eseguito ;) <br>";
+	}
+	else{
+		$contatore = ++$_COOKIE["Biscotto"];
+		setcookie("Biscotto", $contatore);
+		print("Biscotto rilevato! Hai eseguito l'accesso $contatore volte :D <br>");
+	}
+
+	?>
  </body>
 </html>
