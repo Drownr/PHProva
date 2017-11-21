980c6a0b1b5bc10c7d6c0e094df9d9fa9ef24715
Drownr <33448811+Drownr@users.noreply.github.com>
2017-11-21 10:19:49 +0000

    Update correzione.php

--- correzione.php
+++ correzione.php
@@ -6,15 +6,16 @@
 	<?php 
 	if (!isset($_COOKIE["Biscotto"])){
 		$contatore = 1;
-    $scadenza = time() + (86400 * 7);
+    		$scadenza = time() + (86400 * 7);
     
 		setcookie("Biscotto", $contatore, $scadenza);	//86400 sono i secondi in un giorno!
-    setcookie("DataScadenza", $scadenza, time() + (86400 * 7));
+    		setcookie("DataScadenza", $scadenza, time() + (86400 * 7));
     
 		echo "Biscotto settato! Primo accesso eseguito ;) <br>";
 	}
 	else{
 		$contatore = ++$_COOKIE["Biscotto"];
+		$scadenza = $_COOKIE["DataScadenza"];
 		setcookie("Biscotto", $contatore, $scadenza - time());
 		print("Biscotto rilevato! Hai eseguito l'accesso $contatore volte :D <br>");
 	}
