dca5252ee36cd657000dc5223ecd811ba3498ee8
Drownr <33448811+Drownr@users.noreply.github.com>
2017-11-21 10:14:24 +0000

    Create correzione.php

---
+++ correzione.php
@@ -0,0 +1,24 @@
+<html>
+ <head>
+  <title>Contatore Accessi :D</title>
+ </head>
+ <body>
+	<?php 
+	if (!isset($_COOKIE["Biscotto"])){
+		$contatore = 1;
+    $scadenza = time() + (86400 * 7);
+    
+		setcookie("Biscotto", $contatore, $scadenza);	//86400 sono i secondi in un giorno!
+    setcookie("DataScadenza", $scadenza, time() + (86400 * 7));
+    
+		echo "Biscotto settato! Primo accesso eseguito ;) <br>";
+	}
+	else{
+		$contatore = ++$_COOKIE["Biscotto"];
+		setcookie("Biscotto", $contatore, $scadenza - time());
+		print("Biscotto rilevato! Hai eseguito l'accesso $contatore volte :D <br>");
+	}
+
+	?>
+ </body>
+</html>
